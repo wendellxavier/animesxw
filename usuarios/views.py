296f058c37b806807263bef525e6363f936afe0f
wendell <wendell_xw20@hotmail.com>
2024-03-26 10:52:43 -0300

    anime

---
+++ usuarios/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Usuario
+from django.shortcuts import redirect
+from hashlib import sha256
+
+
+def login(request):
+    status = request.GET.get('status')
+    return render(request, 'login/login.html', {'status': status})
+
+def cadastro(request):
+    status = request.GET.get('status')
+    return render(request, 'cadastro/cadastro.html', {'status': status})
+    
+        
+def valida_cadastro(request):
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        if len(nome.strip()) == 0 or len(email.strip()) == 0:
+            return redirect('/auth/cadastro/?status=1')
+        
+        if len(senha) < 4:
+            return redirect('/auth/cadastro/?status=2')
+        
+        usuario = Usuario.objects.filter(email = email)
+        
+        if len(usuario) > 0:
+            return redirect('/auth/cadastro/?status=3')
+        try:
+            senha = sha256(senha.encode()).hexdigest()
+            usuario = Usuario(nome=nome, senha=senha, email=email)
+            usuario.save()
+            return redirect('/auth/cadastro/?status=0')
+        except:
+            return redirect('auth/cadastro/?status=4')
+        
+def valida_login(request):
+    email = request.POST.get('email')
+    senha = request.POST.get('senha')
+    
+    senha = sha256(senha.encode()).hexdigest()
+    usuario = Usuario.objects.filter(email=email).filter(senha=senha)
+    
+    if len(usuario) == 0:
+        return redirect('/auth/login/?status=1')
+    elif len(usuario) > 0:
+        request.session['logado'] = True
+        return redirect('/plataforma/home')
+    
+    
+def sair(request):
+    request.session['logado'] = None
+    return redirect('/auth/login/')
